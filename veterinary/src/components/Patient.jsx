39e0e0673be1f0ea05e34fc749d8e18dd9e7d035
David Hernandez <desarrollo5@albertoalvarez.com>
2024-02-12 19:35:42 -0500

    new project veterinary

---
+++ veterinary/src/components/Patient.jsx
@@ -0,0 +1,23 @@
+const Patient = () => {
+  return (
+    <div className="m-5 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+        <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+        <span className="font-normal normal-case">David</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+        <span className="font-normal normal-case">dh129@gmail.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta: {''}
+        <span className="font-normal normal-case">06 Febrero 2024</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</span>
+        </p>
+    </div>
+  )
+}
+
+export default Patient
